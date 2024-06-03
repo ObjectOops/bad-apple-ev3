aa81e849397199cf88aecff9c95bd79de9b321b5
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-06-02 23:32:23 -0500

    Revert untested changes. This reverts back to 514aff9.

--- midi_parse.py
+++ midi_parse.py
@@ -1,11 +1,11 @@
 import pretty_midi
 
-midi_data = pretty_midi.PrettyMIDI("assets/audio/alstroemeria_records_bad_apple.mid")
+midi_data = pretty_midi.PrettyMIDI('assets/audio/alstroemeria_records_bad_apple.mid')
 
 instrument_count = len(midi_data.instruments)
 
 for i in range(instrument_count):
     instrument = midi_data.instruments[i]
-    with open(f"assets/audio/raw/instrument_{i}.txt", "w+") as raw_out:
+    with open(f'assets/audio/raw/instrument_{i}.txt', 'w+') as raw_out:
         for note in instrument.notes:
-            raw_out.write(f"{note.pitch} {note.velocity} {note.get_duration()} {note.start} {note.end}\n")
+            raw_out.write(f'{note.pitch} {note.velocity} {note.get_duration()} {note.start} {note.end}\n')
--- src/bad-apple-ev3.py
+++ src/bad-apple-ev3.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env pybricks-micropython
+
 """
 The maximum fixed FPS is somewhere around 13 frames per second.
 Using the variable framerate display function might achieve slightly higher results.
@@ -10,9 +12,9 @@ from time import ticks_us, sleep_us
 from pybricks.media.ev3dev import Image
 from pybricks.hubs import EV3Brick
 
-asset_directory = "/home/robot/bad-apple-ev3/assets"
-frame_directory = os.path.join(asset_directory, "frames")
-audio_directory = os.path.join(asset_directory, "audio")
+asset_directory = "/home/robot/bad-apple-ev3/assets/"
+frame_directory = asset_directory + "frames/"
+audio_directory = asset_directory + "audio/"
 video_length = (3 * 60 + 39 + 0.08) * 1_000_000
 variable_fps = True
 frame_count = 6572 # 30 FPS max.
@@ -29,45 +31,48 @@ ev3.light.off()
 
 done = False
 
+def play_audio():
+    ev3.speaker.play_file(audio_directory + "bad-apple-audio.wav")
+
 # Frames are loaded one by one since there's a tough memory limit 
 # and the TI am1808 ARM microprocessor is single core anyways.
 # The C-function that the Pybricks API binds to forces one to use PNGs.
 # The ev3dev Python display bindings are not compatible with MicroPython 
 # and will probably be too slow.
 
-def play_video_fixed_fps(frames: list[str]):
+def play_video_fixed_fps(frames):
     print("Using fixed FPS.")
     global done
     dt = int(video_length / frame_count * fps_reduction - dt_offset)
-    for frame in frames:
+    for i in frames:
         t1 = ticks_us()
-        ev3.screen.load_image(frame)
+        ev3.screen.load_image(i)
         t2 = ticks_us()
         sleep_us(dt - (t2 - t1))
         # print(dt - (t2 - t1))
     done = True
 
-def play_video_variable_fps(frames: list[str]):
+def play_video_variable_fps():
     print("Using variable FPS.")
     global done
     start = ticks_us()
     while not done:
         # Sometimes, the tick function bugs out and returns a value that results in a negative integer.
         # If a runtime error occurs because the frames went slightly too far, then so be it.
-        ev3.screen.load_image(frames[max(0, ticks_us() - start) / video_length * frame_count])
+        ev3.screen.load_image("frame%04d.png" % (max(0, ticks_us() - start) / video_length * frame_count + 1))
 
-def play(audio_function: function):
+if __name__ == "__main__":
     os.chdir(frame_directory)
 
     frames = ["frame%04d.png" % (i * fps_reduction) for i in range(1, int((frame_count + 1) / fps_reduction))]
 
     if variable_fps:
-        video_thread = Thread(target=play_video_variable_fps, args=[frames])
+        video_thread = Thread(target=play_video_variable_fps)
     else:
         video_thread = Thread(target=play_video_fixed_fps, args=[frames])
     video_thread.start()
 
-    audio_function()
+    play_audio()
 
     if variable_fps:
         done = True
